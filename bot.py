d8ca211b0f86c0bfb4c23e515e437f4dcea019a6
ANURAG MISHRA <anuragmishra.ofc@gmail.com>
2021-04-03 22:56:10 +0530

    Added the Bot.py file with Auth keys

---
+++ bot.py
@@ -0,0 +1,9 @@
+import tweepy
+consumer_key = 'IfJZVRHEUajEvQXa3nMdke7cq'
+consumer_secret = 'B2c1wjpJdDS34x5c3wqOgQ3o64KydTWIVWZdiI7Xnhaqr9rU2T'
+key = '1131296400749154304-7JIXZhqfYiEE32m35vQuUCg1R4s8uQ'
+secret = 'PKgsj8ORsebCivylOnM0pbaKqHHv9NKK38Ar3tBFtsx1g'
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(key, secret)
+api = tweepy.API(auth)
+api.update_status('First tweet by Cloudy_bot')
